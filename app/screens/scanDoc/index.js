f9b06e4e0cc91f0b206545e113bb8e9a645f989b
nicholas-bd <nicholas.bond.d@gmail.com>
2020-05-20 23:27:30 +0200

    added IOS version scan doc

--- app/screens/scanDoc/index.js
+++ app/screens/scanDoc/index.js
@@ -1,47 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { View, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Platform } from 'react-native';
 import { normalizeSize } from '@theme/layout';
-import { Icon } from '@components/icon';
-import scanViewBorderImage from '@images/camera/scanview_border.png';
-import scanViewBackButton from '@images/icons/scanview_back_button.png';
-import scanViewCameraButton from '@images/icons/scanview_camera_button.png';
+import ScanPassportAndroid from "./android";
+import ScanPassportIOS from "./ios";
 
 export default function ScanPassport(props) {
   const { navigation } = props;
   return (
       <View style={{ flex: 1 }}>
-          <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}>
-            <Image
-                source={scanViewBorderImage}
-                style={{
-                  height: '76%',
-                  resizeMode: 'contain',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginTop: normalizeSize(40)
-                }} />
-            <View style={styles.buttonGroup}>
-              <TouchableOpacity
-                  style={styles.backButton}
-                  onPress={() => {
-                    navigation.navigate('Home');
-                  }}>
-                <Icon imageUrl={scanViewBackButton} width={96} height={68} />
-              </TouchableOpacity>
-              <TouchableOpacity
-                  style={styles.takePhoto}
-              >
-                <Icon imageUrl={scanViewCameraButton} width={85} height={85} />
-              </TouchableOpacity>
-            </View>
-          </View>
+          {
+              Platform.OS === 'android' ?
+                  <ScanPassportAndroid navigation={navigation}/> :
+                  <ScanPassportIOS navigation={navigation}/>
+          }
       </View>
   );
 }
